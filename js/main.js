d0e402f686871d82460d95f9075d03398a77059f
Yaw <ypinamangyapong@gmail.com>
2025-08-01 14:21:07 +0900

    Include four basic operations

---
+++ js/main.js
@@ -0,0 +1,37 @@
+const add = function (a, b) {
+    return a + b;
+}
+
+const subtract = function (a, b) {
+    return a - b;
+}
+
+const multiply = function (a, b) {
+    return a * b;
+}
+
+const divide = function (a, b) {
+    return a / b;
+}
+
+
+const operate = function (operator, a, b) {
+    switch (operator) {
+        case "+": 
+            return add(a, b);
+            break;
+        case "-":
+            return subtract(a, b);
+            break;
+        case "*":
+            return multiply(a, b);
+            break;
+        case "/":
+            return divide(a, b);
+            break;
+        default: 
+            return "Invalid";
+    }
+}
+
+
